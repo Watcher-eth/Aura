029fb7336415eb91947dc53a5282f8518c3590c0
watcher-eth1 <watchereth10@gmail.com>
2024-12-18 23:46:26 +0100

    fixes

---
+++ packages/nextjs/services/web3/wagmiConnectors.tsx
@@ -0,0 +1,46 @@
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import {
+  coinbaseWallet,
+  ledgerWallet,
+  metaMaskWallet,
+  rainbowWallet,
+  safeWallet,
+  walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { rainbowkitBurnerWallet } from "burner-connector";
+import * as chains from "viem/chains";
+import scaffoldConfig from "~~/scaffold.config";
+
+const { onlyLocalBurnerWallet, targetNetworks } = scaffoldConfig;
+
+const wallets = [
+  metaMaskWallet,
+  walletConnectWallet,
+  ledgerWallet,
+  coinbaseWallet,
+  rainbowWallet,
+  safeWallet,
+  ...(!targetNetworks.some(network => network.id !== (chains.hardhat as chains.Chain).id) || !onlyLocalBurnerWallet
+    ? [rainbowkitBurnerWallet]
+    : []),
+];
+
+/**
+ * wagmi connectors for the wagmi context
+ */
+export const wagmiConnectors = connectorsForWallets(
+  [
+    {
+      groupName: "Supported Wallets",
+      wallets,
+    },
+  ],
+
+  {
+    appName: "Aura",
+    appDescription: "Rate and Review contracts accross all Blockchains",
+    appUrl: "https://family.co", // your app's url
+
+    projectId: scaffoldConfig.walletConnectProjectId,
+  },
+);
